d811d1b33f6d818aad6d3edf879cc0bd48b9036f
Pratyush <pratyush.ghatak@indusnet.co.in>
2022-06-04 10:00:20 +0530

    Child added

--- src/component/ReduxComponents/SubChild2.jsx
+++ src/component/ReduxComponents/SubChild2.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Child2 from './SubChild1'
+import SubChild3 from './SubChild3'
 
 const SubChild2 = () => {
   return (
     <div className='border border-danger p-5'>
+        <div>
+
         SubChild2 of Child1
+        </div>
+        <SubChild3/>
     </div>
   )
 }
---
+++ src/component/ReduxComponents/SubChild3.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SubChild3 = () => {
+  return (
+    <div className='border border-warning p-5'>
+        SubChild of SubChild2
+    </div>
+  )
+}
+
+export default SubChild3
